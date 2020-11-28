0420c428fb9d31687baff5c5dac401fb1a8536a9
tomasv10 <tomasvarnauskas19@gmail.com>
2020-11-28 16:59:52 +0200

    intagrated laravel passport

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,10 @@ use App\Http\Controllers\TodoController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 
 
 Route::get('/todos', [TodoController::class, 'index']);
